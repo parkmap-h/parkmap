0fd5e339c920d4c5afc48724d4f21871a4dee519
Tomohiko Himura <eiel.hal@gmail.com>
2015-02-04 20:35:47 +0900

    環境変数が定義されてない場合の対処

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider                         => 'Google',
-    :google_storage_access_key_id     => ENV['GOOGLE_STORAGE_ID'],
-    :google_storage_secret_access_key => ENV['GOOGLE_STORAGE_KEY']
+    :google_storage_access_key_id     => ENV['GOOGLE_STORAGE_ID'] || '',
+    :google_storage_secret_access_key => ENV['GOOGLE_STORAGE_KEY'] || '',
   }
   config.fog_directory = 'parkmap-image'
 end
