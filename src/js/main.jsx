4d7063604b22b8045bf98859d2edf24942f66068
Tomohiko Himura <eiel.hal@gmail.com>
2015-02-20 20:59:27 +0900

    現状と同機能程度のクライアントを実装

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -2,26 +2,58 @@
 var React = require('react');
 var ReactGoogleMaps = require("react-googlemaps");
 var GoogleMapsAPI = window.google.maps;
+var jquery = require('jquery');
 
 var Map = ReactGoogleMaps.Map;
+var Marker = ReactGoogleMaps.Marker;
 
 var Parkmap = React.createClass({
-  render: function() {
-    return <Map
-       initialZoom={17}
-       initialCenter={new GoogleMapsAPI.LatLng(34.393056, 132.465511)}
-       width={'100%'}
-       height={'100%'}
-     />;
-   }
-});
-
-var Hello = React.createClass({
-  handleClick: function(event) {
-      console.log('hoge');
+  getInitialState: function() {
+    return {
+      parks: [],
+      target: new GoogleMapsAPI.LatLng(34.393056, 132.465511)
+    };
   },
+
+  handleSearch:  function() {
+   var that = this;
+   jquery.post(
+     'http://localhost:3000/.json',
+     {distance: 300,longitude: this.state.target.D, latitude: this.state.target.k},
+     function (data){
+       that.setState({parks: data});
+     }
+   );
+  },
+
+  handleClick: function(e) {
+    console.log(e.latLng);
+    this.setState({target: e.latLng});
+  },
+
+  handleDrag: function(e) {
+    console.log(e.latLng);
+    this.setState({target: e.latLng});
+  },
+
   render: function() {
-    return <div onClick={hoge}>Hello, {this.props.name}!</div>;
+    var marks = this.state.parks.map(function (park) {
+      return (
+        <Marker position={new GoogleMapsAPI.LatLng(park.latitude, park.longitude)} />
+      );});
+    return <div>
+<button onClick={this.handleSearch} className={"search-button"}> 検索 </button>
+<Map
+  initialZoom={17}
+  initialCenter={this.state.target}
+  width={'100%'}
+  height={'100%'}
+  onClick={this.handleClick}
+>
+   <Marker position={this.state.target} opacity={0.5} title={'目的地'} draggable={true} onDrag={this.handleDrag}/>
+  {marks}
+</Map>
+</div>;
   }
 });
 
