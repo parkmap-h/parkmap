8ca2c6321c725fa7a0e44f3eadefa8a758cd84b6
Tomohiko Himura <eiel.hal@gmail.com>
2015-03-12 11:30:58 +0900

    現在地取得ボタンを作成

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,5 @@
 "use strict";
-var React = require('react');
+var React = require('react/addons');
 var ReactGoogleMaps = require("react-googlemaps");
 var GoogleMapsAPI = window.google.maps;
 var jquery = require('jquery');
@@ -36,41 +36,57 @@ var Park = React.createClass({
 });
 
 var Parkmap = React.createClass({
+  mixins: [React.addons.LinkedStateMixin],
   getInitialState: function() {
     return {
       parks: [],
-      target: new GoogleMapsAPI.LatLng(34.393056, 132.465511)
+      target: new GoogleMapsAPI.LatLng(34.393056, 132.465511),
+      center: new GoogleMapsAPI.LatLng(34.393056, 132.465511),
     };
   },
 
   handleSearch:  function() {
     var that = this;
-    jquery.post(
-      baseurl + '/.json',
-      {
-        distance: 300,
-        longitude: this.state.target.D,
-        latitude: this.state.target.k
-      },
-      function (data){
-        that.setState(
-          {
-            parks: data.features.sort(function(feature_a,feature_b) {
-              var park_a = feature_a.properties;
-              var park_b = feature_b.properties;
-              return park_a.distance - park_b.distance;
-            })
-          }
-        );
-      }
-    );
+    var success = function (data){
+      that.setState(
+        {
+          parks: data.features.sort(function(feature_a,feature_b) {
+            var park_a = feature_a.properties;
+            var park_b = feature_b.properties;
+            return park_a.distance - park_b.distance;
+          })
+        }
+      );
+    };
+    var data = {
+      distance: 300,
+      longitude: this.state.target.D,
+      latitude: this.state.target.k
+    };
+    jquery.post(baseurl + '/.json',data,success);
+  },
+
+  handlePresentLocation: function(e) {
+    var that = this;
+    var success = function (e) {
+      var latlng = new GoogleMapsAPI.LatLng(e.coords.latitude, e.coords.longitude);
+      that.setState({center: latlng, target: latlng});
+    };
+    var error = function () {
+      // TODO
+    };
+    navigator.geolocation.getCurrentPosition(success,error,{});
+  },
+
+  handleCenterChange: function(e) {
+    this.setState({center: e.getCenter()});
   },
 
   handleClick: function(e) {
     this.setState({target: e.latLng});
   },
 
-  handleDrag: function(e) {
+  handleMarkerDrag: function(e) {
     this.setState({target: e.latLng});
   },
 
@@ -96,17 +112,21 @@ var Parkmap = React.createClass({
       marks = (<p className="help">目的地を設定して検索をしてください。</p>)
     }
     return <div>
-      <button onClick={this.handleSearch} className={"search-button"}>
+      <button className="location-button" onClick={this.handlePresentLocation}>
+        現在地
+      </button>
+      <button className="search-button" onClick={this.handleSearch}>
         検索
       </button>
       <Map
         initialZoom={16}
-        initialCenter={this.state.target}
+        center={this.state.center}
+        onCenterChange={this.handleCenterChange}
         width={'100%'}
         height={'100%'}
         onClick={this.handleClick}
         >
-        <Marker position={this.state.target} opacity={0.5} title={'目的地'}    draggable={true} onDrag={this.handleDrag}/>
+        <Marker position={this.state.target} opacity={0.5} title={'目的地'}    draggable={true} onDrag={this.handleMarkerDrag}/>
         {marks}
       </Map>
       <div className={"parks"}>
