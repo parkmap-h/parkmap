614b86b592897b4e3b0c2db194ddc0352d14464f
Tomohiko Himura <eiel.hal@gmail.com>
2015-04-09 10:02:36 +0900

    料金情報のサンプルのJSONを編集画面についか
    コピーしにいくのがめんどうなのでリンクと例を追加した

--- app/helpers/parks_helper.rb
+++ app/helpers/parks_helper.rb
@@ -1,2 +1,27 @@
 module ParksHelper
+  def sample_fee_json
+    <<JSON
+{
+  "type":"times",
+  "times":[
+    {
+       "type":"basic",
+       "start":{"hour":7,"min":0},
+       "end":{"hour":22,"min":0},
+       "per_minute":30,
+       "fee":180,
+       "max":1200
+    },
+    {
+       "type":"basic",
+       "start":{"hour":22,"min":0},
+       "end":{"hour":7,"min":0},
+       "per_minute":30,
+       "fee":180,
+       "max":1200
+    }
+  ]
+}
+JSON
+  end
 end
