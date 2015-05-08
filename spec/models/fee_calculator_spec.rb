b2372fdf25d2a1ec3a0e202c2454e3b2865eec1d
Tomohiko Himura <eiel.hal@gmail.com>
2015-05-08 22:10:28 +0900

    12時間以内ならn円に対応
    issue #80

--- spec/models/fee_calculator_spec.rb
+++ spec/models/fee_calculator_spec.rb
@@ -59,6 +59,50 @@ RSpec.describe FeeCalculator, :type => :model do
       it { is_expected.to eq(400) }
     end
 
+    context '12時間以内1200円' do
+      let(:attr) do
+        {
+         'type' => 'within',
+         'minute' => 720,
+         'fee' => 1200,
+         'repeat' => false,
+         'within' => {
+           'type' => 'basic',
+           'per_minute' => 60,
+           'fee' => fee,
+         }
+        }
+      end
+
+      context '1時間3000円' do
+        let(:fee) { 3000 }
+        it { is_expected.to eq(1200) }
+      end
+
+      context '1時間100円' do
+        let(:fee) { 100 }
+        it { is_expected.to eq(100) }
+      end
+    end
+
+    context '12時間以内1200円' do
+      let(:attr) do
+        {
+         'type' => 'within',
+         'minute' => 720,
+         'fee' => 1200,
+         'repeat' => false,
+         'within' => {
+           'type' => 'basic',
+           'per_minute' => 60,
+           'fee' => 3000,
+         }
+        }
+      end
+
+      it { is_expected.to eq(1200) }
+    end
+
     context '20:00から8:00 まで60分200円 8:00から20:00 まで20分200円' do
       let(:attr) do
         {
